8ef307d507454f606d7f4c535a7ab14f6ca06b09
0xerasmus <ofyuakwrov7367@hotmail.com>
2025-10-17 14:59:04 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -11,7 +11,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   console.log(`FHECounter contract: `, deployedFHECounter.address);
+
+  const deployedEncryptedMessenger = await deploy("EncryptedMessenger", {
+    from: deployer,
+    log: true,
+  });
+  console.log(`EncryptedMessenger contract: `, deployedEncryptedMessenger.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_contracts"; // id required to prevent reexecution
+func.tags = ["FHECounter", "EncryptedMessenger"];
